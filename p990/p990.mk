5977b8e8d7beedfa1d22136d4b426a94aab2718f
Domenico <nospamdomi@hotmail.it>
2012-07-03 18:47:11 +0200

    LG P990 Device Tree for TWRP

---
+++ p990/p990.mk
@@ -0,0 +1,32 @@
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_eu.mk)
+
+$(call inherit-product, device/lge/star-common/star.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/lge/p990/overlay
+
+# Inherit non-open-source blobs.
+$(call inherit-product-if-exists, vendor/lge/p990/p990-vendor.mk)
+
+# Board-specific init
+PRODUCT_COPY_FILES += \
+    device/lge/p990/init.p990.rc:root/init.p990.rc \
+    $(LOCAL_PATH)/ueventd.tegra.rc:root/ueventd.p990.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
+    $(LOCAL_PATH)/init.vsnet:system/bin/init.vsnet \
+    $(LOCAL_PATH)/gps_brcm_conf.xml:system/etc/gps_brcm_conf.xml
+
+PRODUCT_PACKAGES += \
+    lights.p990 \
+    audio.primary.p990
+    #audio_policy.p990
+
+# Camera
+PRODUCT_PACKAGES += \
+    Camera
+
+PRODUCT_NAME := full_p990
+PRODUCT_DEVICE := p990
+PRODUCT_MODEL := LG-P990
